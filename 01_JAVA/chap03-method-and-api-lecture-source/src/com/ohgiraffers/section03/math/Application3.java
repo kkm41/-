d375dbc0aca10251bcea7b73a9fc314811f9abff
JJS <163955185+kkm41@users.noreply.github.com>
2024-03-22 16:17:45 +0900

    메소드

---
+++ 01_JAVA/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section03/math/Application3.java
@@ -0,0 +1,36 @@
+package src.com.ohgiraffers.section03.math;
+
+import java.util.Random;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+        /*수업목표, java.util.Random 클래스를 활용하여 사용자 범위의 난수를 발생시킬 수 있다
+        *
+        * java.util.Random 클래스
+        * java.util.Random 클래스 nextInt()메소드를 이용한 난수 발생
+        * nextInt(int bound) : 0부터 매개변수로 전달 받은 정수 범위까지의 난수를 발생시켜서 정수 형태로 반환
+        * */
+
+        /*random.nextInt(구하려는 난수의 객수) + 구하려는 난수의 최소값*/
+
+        Random ran = new Random(); //import 가져옴
+
+        /*1. 0부터 9까지 난수발생 */
+        int randomNum = ran.nextInt(10);
+        System.out.println("0부터 9까지의 난수 ? : " + randomNum);
+
+        /*2. 1부터 10까지 난수발생 */
+        int randomNum2 = ran.nextInt(10)+1;
+        System.out.println("1부터 10까지의 난수 ? : " + randomNum2);
+
+        /*3. 20부터 45까지 난수발생*/
+        int randomNum3 = ran.nextInt(26) +20;
+        System.out.println("20부터 45까지의 난수 ? :" + randomNum3);
+
+        /* 4. -128 ~ 127까지 난수발생 */
+        //int randomNum4 = ran.nextInt(256) - 128;
+        int randomNum5 = new Random().nextInt(256) - 128; //객체를 생성하자마자 바로 메소드 호출 가능
+        System.out.println("-128 ~ 127까지의 난수발생 " + randomNum5);
+    }
+}
